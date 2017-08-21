7507d8376922f29da3272fcd3e4b0986b2d4a309
Morgan Bazalgette <the@howl.moe>
2017-08-21 09:20:56 +0200

    Remove inactive users from leaderboard

--- redis.go
+++ redis.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"math"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
 	redis "gopkg.in/redis.v5"
@@ -26,7 +28,13 @@ func opPopulateRedis() {
 
 	r.Del("hanayo:country_list")
 
-	const initQuery = "SELECT users_stats.id, users_stats.country, pp_std, ranked_score_taiko, ranked_score_ctb, pp_mania FROM users_stats INNER JOIN users ON users.id = users_stats.id WHERE privileges & 1 > 0"
+	const initQuery = `
+SELECT
+	users_stats.id, users_stats.country, pp_std,
+	ranked_score_taiko, ranked_score_ctb, pp_mania,
+	playcount_std, playcount_taiko, playcount_ctb, playcount_mania,
+	users.latest_activity
+FROM users_stats INNER JOIN users ON users.id = users_stats.id WHERE privileges & 1 > 0`
 
 	rows, err := db.Query(initQuery)
 	if err != nil {
@@ -34,13 +42,22 @@ func opPopulateRedis() {
 		return
 	}
 
+	currentSeconds := time.Now().Unix()
+
 	var (
-		uid     int
-		country string
-		pp      [4]int64
+		uid            int
+		country        string
+		pp             [4]int64
+		playcount      [4]int
+		latestActivity int64
 	)
 	for rows.Next() {
-		err = rows.Scan(&uid, &country, &pp[0], &pp[1], &pp[2], &pp[3])
+		err = rows.Scan(
+			&uid, &country, &pp[0],
+			&pp[1], &pp[2], &pp[3],
+			&playcount[0], &playcount[1], &playcount[2], &playcount[3],
+			&latestActivity,
+		)
 		if err != nil {
 			queryError(err, initQuery)
 			continue
@@ -53,6 +70,9 @@ func opPopulateRedis() {
 		}
 
 		for k, v := range pp {
+			if isInactive(float64(currentSeconds-latestActivity), playcount[k]) {
+				continue
+			}
 			r.ZAdd("ripple:leaderboard:"+modes[k], redis.Z{
 				Member: uid,
 				Score:  float64(v),
@@ -67,4 +87,9 @@ func opPopulateRedis() {
 	}
 
 	color.Green("> PopulateRedis: done!")
+}
+
+func isInactive(secondsInactive float64, playcount int) bool {
+	daysInactive := secondsInactive / (60 * 60 * 24)
+	return daysInactive > (math.Log(float64(playcount)) * 16)
 }
