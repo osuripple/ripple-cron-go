f8ff58058e851cdff830309c30bf95b54e1fc406
Giuseppe Guerra <to@nyo.zz.mu>
2020-10-01 17:34:52 +0200

    Run fix stats overflow for both classic and relax stats

--- fix_stats_overflow.go
+++ fix_stats_overflow.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/fatih/color"
 )
 
-func opFixStatsOverflow() {
+func fixStatsOverflow(relax bool) {
 	defer wg.Done()
-	const initQuery = `SELECT id FROM users_stats JOIN users USING(id) WHERE 
+	var table string
+	if relax {
+		table = "users_stats_relax"
+	} else {
+		table = "users_stats"
+	}
+	initQuery := fmt.Sprintf(`SELECT id FROM %s JOIN users USING(id) WHERE 
 	ranked_score_std < 0 OR 
 	ranked_score_taiko < 0 OR 
 	ranked_score_ctb < 0 OR 
-	ranked_score_mania < 0`
+	ranked_score_mania < 0`, table)
 	rows, err := db.Query(initQuery)
 	if err != nil {
 		queryError(err, initQuery)
@@ -29,8 +37,12 @@ func opFixStatsOverflow() {
 		if usersCount%1000 == 0 {
 			verboseln("> FixStatsOverflow::users:", usersCount)
 		}
-		const fetchQuery = "SELECT score, play_mode FROM scores JOIN beatmaps USING(beatmap_md5) WHERE userid = ? AND completed = 3"
-		scoreRows, err := db.Query(fetchQuery, uid)
+		const fetchQuery = "SELECT score, play_mode FROM scores JOIN beatmaps USING(beatmap_md5) WHERE userid = ? AND completed = 3 AND is_relax = ?"
+		var relaxV int
+		if relax {
+			relaxV = 1
+		}
+		scoreRows, err := db.Query(fetchQuery, uid, relaxV)
 		if err != nil {
 			queryError(err, fetchQuery, uid)
 			continue
@@ -65,7 +77,12 @@ func opFixStatsOverflow() {
 		if v == nil {
 			continue
 		}
-		op("UPDATE users_stats SET ranked_score_std = ?, ranked_score_taiko = ?, ranked_score_ctb = ?, ranked_score_mania = ? WHERE id = ? LIMIT 1", v[0], v[1], v[2], v[3], uid)
+		op(fmt.Sprintf("UPDATE %s SET ranked_score_std = ?, ranked_score_taiko = ?, ranked_score_ctb = ?, ranked_score_mania = ? WHERE id = ? LIMIT 1", table), v[0], v[1], v[2], v[3], uid)
 	}
 	color.Green("> FixStatsOverflow: done!")
+}
+
+func opFixStatsOverflow() {
+	fixStatsOverflow(false)
+	fixStatsOverflow(true)
 }
